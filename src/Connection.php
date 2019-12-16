64b8f1a24700953ae2e65f2a02f620cafd57d1e0
Yudhi Oktavian Gule <devraxtorchubheq@gmail.com>
2019-12-16 14:51:19 +0700

    CHANGE

--- src/Connection.php
+++ src/Connection.php
@@ -3,5 +3,15 @@
 namespace SQLGen;
 
 class Connection {
-	
+	private $db;
+	function __construct(
+		$options=[
+			"host"=>"localhost",
+			"user"=>'root',
+			"password"=>"",
+			"db"=>"db_data"
+		]){
+		$db = new PDO("mysql:host=$options[host];dbname=$options");
+		$this->db = $db;
+	}
 }
---
+++ test/app.php
@@ -0,0 +1,12 @@
+<?php 
+ require_once("../src/Connection.php");
+ use SQLGen\Connection;
+
+ $connect = new Connection([
+ 	"host"=>"localhost",
+ 	"user"=>"root",
+ 	"pass"=>"123",
+ 	"db" => "db_mdcl"
+ ]);
+
+	
