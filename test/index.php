814fd25065b6f3d1bb1c51fa1d7ceb07213cdace
Yudhi Oktavian Gule <devraxtorchubheq@gmail.com>
2019-12-16 17:43:20 +0700

    fix index.php

--- test/index.php
+++ test/index.php
@@ -4,4 +4,7 @@
 
  $sqlGenerator = new SQL("members");
  $sql = $sqlGenerator->update(['id'=>1,"name"=>"adamas"])->where('id',1)->sql();
+ $sql1 = $sqlGenerator->select(['id','name'])->where('id',1)->sql();
 
+ var_dump($sql);
+ var_dump($sql1);
